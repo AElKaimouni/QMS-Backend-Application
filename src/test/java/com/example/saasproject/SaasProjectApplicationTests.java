5e3e3c0a2eb43338ccca6ec7c2ce160888200562
saraamas <saramasmoudi2001@gmail.com>
2024-10-01 22:40:49 +0100

    first commit

---
+++ src/test/java/com/example/saasproject/SaasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saasproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
