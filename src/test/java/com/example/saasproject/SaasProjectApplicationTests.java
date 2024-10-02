2750b54257a8acfb9b498a27a787561400aaae0c
saraamas <saramasmoudi2001@gmail.com>
2024-10-02 20:42:20 +0100

    postgres+docker

--- src/test/java/com/example/saasproject/SaasProjectApplicationTests.java
+++ src/test/java/com/example/saasproject/SaasProjectApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.saasproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class SaasProjectApplicationTests {
+    @Autowired
+    private Environment env;
 
     @Test
     void contextLoads() {
+        System.out.println(env.getProperty("spring.datasource.url"));
     }
 
 }
